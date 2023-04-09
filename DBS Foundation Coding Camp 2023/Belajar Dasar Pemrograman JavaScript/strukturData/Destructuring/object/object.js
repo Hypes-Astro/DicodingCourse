df1b2e9e726747c8ba8b13daf0b5ecf651c6e191
Hypes-Astro <muhamadalifanwar@gmail.com>
2023-04-09 13:52:15 +0700

    Lanjut belajar

---
+++ DBS Foundation Coding Camp 2023/Belajar Dasar Pemrograman JavaScript/strukturData/Destructuring/object/object.js
@@ -0,0 +1,11 @@
+const profile = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 18
+  }
+   
+  const { firstName: localFirstName, lastName: localLastName, age: localAge } = profile;
+   
+  console.log(localFirstName);
+  console.log(localLastName);
+  console.log(localAge);
